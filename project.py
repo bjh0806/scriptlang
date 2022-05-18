505c0bc88c91e3ded87f14c03f844d6de7b3834d
배지현 <85167224+bjh0806@users.noreply.github.com>
2022-05-18 23:58:59 +0900

    체크 버튼 배치

--- project.py
+++ project.py
@@ -5,15 +5,22 @@ def InitScreen():
     fontTitle = font.Font(g_Tk, size=14, weight='bold')
     frameTitle = Frame(g_Tk, padx=10, pady=10)
     frameTitle.pack(side="top", fill="x")
-    # frameCombo = Frame(g_Tk, padx=10, pady=10, bg='orange')
-    # frameCombo.pack(side="top", fill="x")
     frameEntry = Frame(g_Tk, padx=10, pady=10)
     frameEntry.pack(side="top", fill="x")
+    frameCheck = Frame(g_Tk, padx=10, pady=10, bg='orange')
+    frameCheck.pack(side="top", fill="x")
     frameList = Frame(g_Tk, padx=10, pady=10, bg='green')
     frameList.pack(side="bottom", fill="both", expand=True)
 
     MainText = Label(frameTitle, font=fontTitle, text="경기도 동물 시설 검색 APP")
     MainText.pack(anchor="center", fill="both")
+    
+    global InputLabel
+    InputLabel = Entry(frameEntry, width=40, borderwidth=12, relief='ridge')
+    InputLabel.pack(side="left", expand=True)
+
+    SearchButton = Button(frameEntry, text="검색")
+    SearchButton.pack(side="right", expand=True, fill="both")
 
     # global SearchListBox
     # LBScrollbar = Scrollbar(frameCombo)
@@ -26,13 +33,12 @@ def InitScreen():
     # LBScrollbar.config(command=SearchListBox.yview)
     # sendEmailButton = Button(frameCombo, text='이메일')
     # sendEmailButton.pack(side='right', padx=10, fill='y')
-    
-    global InputLabel
-    InputLabel = Entry(frameEntry, width=40, borderwidth=12, relief='ridge')
-    InputLabel.pack(side="left", expand=True)
 
-    SearchButton = Button(frameEntry, text="검색")
-    SearchButton.pack(side="right", expand=True, fill="both")
+    chkValue = []
+    strCheck = ['약국', '유기 동물 보호소', '장묘 업체', '기타']
+    for i, s in enumerate(strCheck):
+        chkValue.append(IntVar())
+        Checkbutton(frameCheck, text=s, variable=chkValue[i]).pack(side="left")
 
     SubText3 = Label(frameList, text="test4")
     SubText3.pack(anchor="center", fill="both")
