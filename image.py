ad99ca305b8f2bc3d0240f2038786ad48921d19a
배지현 <85167224+bjh0806@users.noreply.github.com>
2022-06-10 00:07:09 +0900

    이미지 모듈 분리

---
+++ image.py
@@ -0,0 +1,69 @@
+from tkinter import *
+
+class ImageLabel(Label):
+    def __init__(self, parent, filenameOrUrl=None, width=0, height=0):
+        super().__init__(parent)
+        if width:
+            self.width = width
+        if height:
+            self.height = height
+        if filenameOrUrl:
+            self.setImage(filenameOrUrl)
+    def setImage(self, flienameOrUrl):
+        from PIL import Image, ImageTk
+        if flienameOrUrl.startswith('http'):
+            from io import BytesIO
+            import urllib.request
+
+            url = flienameOrUrl
+            try:
+                with urllib.request.urlopen(url) as u:
+                    raw_data = u.read()
+            except urllib.error.URLError:
+                print('urllib.error.URLError!')
+                return
+
+            im = Image.open(BytesIO(raw_data))
+        elif flienameOrUrl:
+            im = Image.open(flienameOrUrl)
+
+        im = im.resize((self.width, self.height), Image.ANTIALIAS)
+        img = ImageTk.PhotoImage(im)
+
+        self.configure(image = img)
+
+        self.image = img
+
+class ImageButton(Button):
+    def __init__(self, parent, filenameOrUrl=None, width=0, height=0):
+        super().__init__(parent)
+        if width:
+            self.width = width
+        if height:
+            self.height = height
+        if filenameOrUrl:
+            self.setImage(filenameOrUrl)
+    def setImage(self, flienameOrUrl):
+        from PIL import Image, ImageTk
+        if flienameOrUrl.startswith('http'):
+            from io import BytesIO
+            import urllib.request
+
+            url = flienameOrUrl
+            try:
+                with urllib.request.urlopen(url) as u:
+                    raw_data = u.read()
+            except urllib.error.URLError:
+                print('urllib.error.URLError!')
+                return
+
+            im = Image.open(BytesIO(raw_data))
+        elif flienameOrUrl:
+            im = Image.open(flienameOrUrl)
+
+        im = im.resize((self.width, self.height), Image.ANTIALIAS)
+        img = ImageTk.PhotoImage(im)
+
+        self.configure(image = img)
+
+        self.image = img
